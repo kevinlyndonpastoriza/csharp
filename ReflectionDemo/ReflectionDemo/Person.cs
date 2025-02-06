2d2aba0c0e99de966568b6082bdcf1feb8a60b0e
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-02-05 18:39:04 -0800

    Adding ReflectionDemo

---
+++ ReflectionDemo/ReflectionDemo/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Greet()
+        {
+            return $"Hello, my name is {Name} and I am {Age} years old.";
+        }
+    }
+}
---
+++ ReflectionDemo/ReflectionDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ReflectionDemo;
+using System.Reflection;
+
+// Get type information using reflection
+Type type = typeof(Person);
+
+Console.WriteLine($"Class Name: {type.Name}");
+
+// Display properties
+Console.WriteLine("Properties:");
+foreach (PropertyInfo prop in type.GetProperties())
+{
+    Console.WriteLine($"- {prop.Name} ({prop.PropertyType.Name})");
+}
+
+//Display methods
+Console.WriteLine("\nMethods:");
+foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+{
+    Console.WriteLine($"- {method.Name} ({method.ReturnType.Name})");
+    method.Invoke(null, new object[] { type });
+}
+
+//Dynamically create an instance of Person
+object obj = Activator.CreateInstance(type, new object[] { "John Doe", 30 });
+
+// Invoke a method dynamically
+MethodInfo greetMethod = type.GetMethod("Greet");
+if( greetMethod != null )
+{
+    string result = (string)greetMethod.Invoke(obj, null);
+    Console.WriteLine($"\nDynamically Invoked Method Ouput: {result}");
+}

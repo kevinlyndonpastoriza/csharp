fe29d4be26a705e292b15195d2eb02e649f5791a
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-03-05 04:53:22 -0800

    TwoSumExercise

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TwoSum;
+
+Solution solution = new Solution();
+
+int[] test1 = solution.TwoSumBruteForce(new int[] { 2, 7, 11, 15 }, 9);
+var test2 = solution.TwoSumBruteForce(new int[] { 3, 2, 4 }, 6);
+var test3 = solution.TwoSumBruteForce(new int[] { 3, 3 }, 6);
+
+Console.WriteLine($"{test1[0]}, {test1[1]} ");
+Console.WriteLine($"{test2[0]}, {test2[1]} ");
+Console.WriteLine($"{test3[0]}, {test3[1]} ");
+
+var test4 = solution.TwoSumTwoPointers(new int[] { 2, 7, 11, 15 }, 9);
+var test5 = solution.TwoSumTwoPointers(new int[] { 3, 2, 4 }, 6);
+var test6 = solution.TwoSumTwoPointers(new int[] { 3, 3 }, 6);
+
+Console.WriteLine($"{test4[0]}, {test4[1]} ");
+Console.WriteLine($"{test5[0]}, {test5[1]} ");
+Console.WriteLine($"{test6[0]}, {test6[1]} ");
+
+var test7 = solution.TwoSumHashMap(new int[] { 2, 7, 11, 15 }, 9);
+var test8 = solution.TwoSumHashMap(new int[] { 3, 2, 4 }, 6);
+var test9 = solution.TwoSumHashMap(new int[] { 3, 3 }, 6);
+
+Console.WriteLine($"{test7[0]}, {test7[1]} ");
+Console.WriteLine($"{test8[0]}, {test8[1]} ");
+Console.WriteLine($"{test9[0]}, {test9[1]} ");
---
+++ TwoSum/TwoSum/Solution.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    internal class Solution
+    {
+        public int[] TwoSumBruteForce(int[] nums, int target)
+        {
+
+            for (int i=0; i < nums.Length; i++)
+            {
+                for (int j=i+1; j < nums.Length; i++) // Compare every pair
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { }; // Should never reach here as per problem constraints
+        }
+        public int[] TwoSumTwoPointers(int[] nums, int target)
+        {
+            var sortedNums = nums
+                .Select((value, index) => new {Value = value, Index = index}) // Store original indices
+                .OrderBy(x => x.Value)
+                .ToArray();
+
+            int left = 0;
+            int right = sortedNums.Length - 1;
+
+            while (left < right) 
+            { 
+                int sum = sortedNums[left].Value + sortedNums[right].Value;
+                if (sum == target)
+                {
+                    return new int[] { sortedNums[left].Index, sortedNums[right].Index };
+                } else if (sum < target)
+                {
+                    left++;
+                } else
+                {
+                    right--;
+                }
+            }
+
+            return new int[] { }; // Should never reach here
+        }
+
+        public int[] TwoSumHashMap(int[] nums, int target)
+        {
+            Dictionary<int, int> numMap = new Dictionary<int, int>(); // Stores number -> index
+
+            for (int i=0; i<nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (numMap.ContainsKey(complement)) // If complement exists, return indices
+                {  
+                    return new int[] { numMap[complement], i };
+                }
+
+                numMap[nums[i]] = i; // Store current number with its index
+            }
+
+            return new int[] { }; // Should never reach here
+        }
+
+    }
+
+  
+}
